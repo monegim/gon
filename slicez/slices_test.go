b3e755976ff417fe4567d9246d25a197c1057071
Mostafa Negim <mostafa.negim@gmail.com>
2022-12-17 14:10:58 +0330

    feat: add TestEqualFuncNotDifferentLength

--- slicez/slices_test.go
+++ slicez/slices_test.go
@@ -1,10 +1,24 @@
 package slicez
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestEqualFunc(t *testing.T) {
 	s1 := []int{1, 2, 3}
-	s2 := []int{1, 0, 3}
-	res := EqualFunc(s1, s2, func(a,b int) bool {return a==b })
-	t.Log(res)
+	s2 := []int{1, 2, 3}
+	res := EqualFunc(s1, s2, func(a, b int) bool { return a == b })
+	if !res {
+		t.Fail()
+		t.Log(res)
+	}
+}
+func TestEqualFuncNotDifferentLength(t *testing.T) {
+	s1 := []int{1, 2, 3}
+	s2 := []int{0, 1, 2, 3}
+	res := EqualFunc(s1, s2, func(a, b int) bool { return a == b })
+	if res {
+		t.Fail()
+		t.Log(res)
+	}
 }
