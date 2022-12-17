4e4f5dbc34f49fa83120bd866b241faa56d9a27a
Mostafa Negim <mostafa.negim@gmail.com>
2022-12-17 13:48:48 +0330

    feat: add EqualFunc

---
+++ slicez/slices.go
@@ -0,0 +1,13 @@
+package slicez
+
+func EqualFunc[E1, E2 any](s1 []E1, s2 []E2, eq func(E1, E2) bool) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i, v1 := range s1 {
+		if !eq(v1, s2[i]) {
+			return false
+		}
+	}
+	return true
+}
