afad6541ee221fa5e86d477510b4fb61077ba1d6
Mostafa Negim <mostafa.negim@gmail.com>
2022-12-22 09:22:58 +0330

    add Equal func

--- slicez/slices.go
+++ slicez/slices.go
@@ -1,5 +1,10 @@
 package slicez
 
+import "github.com/monegim/gon/compare"
+
+func Equal[A comparable](s1, s2 []A) bool {
+	return EqualFunc(s1, s2, compare.Equal[A])
+}
 func EqualFunc[E1, E2 any](s1 []E1, s2 []E2, eq func(E1, E2) bool) bool {
 	if len(s1) != len(s2) {
 		return false
